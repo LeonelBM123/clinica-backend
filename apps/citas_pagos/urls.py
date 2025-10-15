0afdfa31e3859fd6f2243eafaf5435338c020b8f
Viveros123 <rodrigoedgarviveros@gmail.com>
2025-10-15 15:51:36 -0400

    Actualización de serializers, urls y views en citas_pagos

--- apps/citas_pagos/urls.py
+++ apps/citas_pagos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import CitaMedicaViewSet, PacientesPorGrupoView, CitasPorPacienteView, CitaDetalleView
+from .views import CitaMedicaViewSet
 
 
 router = DefaultRouter()
@@ -9,7 +9,4 @@ router.register('citas', CitaMedicaViewSet, basename='cita-medica')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('pacientes/', PacientesPorGrupoView.as_view(), name='pacientes-por-grupo'),
-    path('pacientes/<int:paciente_id>/citas/', CitasPorPacienteView.as_view(), name='citas-por-paciente'),
-    path('citas/<int:cita_id>/detalle/', CitaDetalleView.as_view(), name='detalle-cita'),
 ]
