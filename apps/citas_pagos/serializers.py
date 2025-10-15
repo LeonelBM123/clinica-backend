35743992c3f8bcca28af08d0db3cfda76e3b60a5
Viveros123 <rodrigoedgarviveros@gmail.com>
2025-10-15 02:59:08 -0400

    HistorialClinico

--- apps/citas_pagos/serializers.py
+++ apps/citas_pagos/serializers.py
@@ -14,3 +14,52 @@ class CitaMedicaSerializer(serializers.ModelSerializer):
                   'notas', 
                   'fecha_creacion', 
                   'fecha_modificacion']
+
+
+class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paciente
+        fields = ['id', 'nombre', 'apellido', 'ci', 'telefono']
+
+
+class CitaMedicaListSerializer(serializers.ModelSerializer):
+    medico_nombre = serializers.CharField(source='bloque_horario.medico.nombre', read_only=True)
+    medico_apellido = serializers.CharField(source='bloque_horario.medico.apellido', read_only=True)
+
+    class Meta:
+        model = Cita_Medica
+        fields = [
+            'id',
+            'fecha',
+            'hora_inicio',
+            'hora_fin',
+            'estado',
+            'notas',
+            'medico_nombre',
+            'medico_apellido',
+        ]
+
+
+class CitaMedicaDetalleSerializer(serializers.ModelSerializer):
+    paciente_nombre = serializers.CharField(source='paciente.nombre', read_only=True)
+    paciente_apellido = serializers.CharField(source='paciente.apellido', read_only=True)
+    medico_nombre = serializers.CharField(source='bloque_horario.medico.nombre', read_only=True)
+    medico_apellido = serializers.CharField(source='bloque_horario.medico.apellido', read_only=True)
+
+    class Meta:
+        model = Cita_Medica
+        fields = [
+            'id',
+            'fecha',
+            'hora_inicio',
+            'hora_fin',
+            'estado',
+            'notas',
+            'motivo_cancelacion',
+            'paciente_nombre',
+            'paciente_apellido',
+            'medico_nombre',
+            'medico_apellido',
+            'fecha_creacion',
+            'fecha_modificacion'
+        ]
--- apps/citas_pagos/urls.py
+++ apps/citas_pagos/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import CitaMedicaViewSet, PacientesPorGrupoView, CitasPorPacienteView, CitaDetalleView
+
 
 router = DefaultRouter()
-
-router.register(r'citas-medicas', views.CitaMedicaViewSet)
+router.register('citas', CitaMedicaViewSet, basename='cita-medica')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('pacientes/', PacientesPorGrupoView.as_view(), name='pacientes-por-grupo'),
+    path('pacientes/<int:paciente_id>/citas/', CitasPorPacienteView.as_view(), name='citas-por-paciente'),
+    path('citas/<int:cita_id>/detalle/', CitaDetalleView.as_view(), name='detalle-cita'),
 ]
