45eb31188d96abb5876d8b7ee99bd438c717b3a0
Leonel Barrios <leonel.barriosmay@gmail.com>
2025-10-16 07:07:23 -0400

    asdf

--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "https://clinica-frontend-six.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
