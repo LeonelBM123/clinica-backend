dedc38a8d9fc2f8d320799892426ccf9d80e1ccd
Leonel Barrios <leonel.barriosmay@gmail.com>
2025-10-16 05:25:25 -0400

    pagos implementados

--- apps/citas_pagos/urls.py
+++ apps/citas_pagos/urls.py
@@ -6,6 +6,9 @@ router = DefaultRouter()
 
 router.register(r'citas-medicas', views.CitaMedicaViewSet)
 
+
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('create-payment-intent/', views.create_payment_intent, name='create-payment-intent'),
 ]
